8c077ff2c4e01cc054d8f4303f6a910a638d1aa7
francis frias <francisr.frias@gmail.com>
2024-03-16 12:04:21 +0800

    added the desktop image

--- app/_components/RegisterForm.tsx
+++ app/_components/RegisterForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { ChangeEventHandler, FormEvent, useState } from 'react';
-import coverImage from '@/public/assets/images/illustration-sign-up-mobile.svg';
+import coverImageMobile from '@/public/assets/images/illustration-sign-up-mobile.svg';
+import coverImageDesktop from '@/public/assets/images/illustration-sign-up-desktop.svg';
 import Image from 'next/image';
 import Descriptions from './Descriptions';
 import Textfield from './Textfield';
@@ -35,41 +36,49 @@ const RegisterForm = () => {
   };
 
   return (
-    <div className='h-screen overflow-auto'>
-      <Image
-        src={coverImage}
-        alt='Cover Image'
-        priority
-        width='0'
-        height='0'
-        sizes='100vw'
-        className='md:hidden sm:block w-full h-auto p-0 m-auto'
-      />
-      <div className='px-6 py-8 space-y-6 '>
-        <h1 className='text-4xl text-neutralDarkSlateGrey font-bold'>
-          Stay updated!
-        </h1>
-        <h5 className='text-neutralDarkSlateGrey'>
-          Join 60,000+ product managers receiving monthly updates on:
-        </h5>
-        <div className='flex flex-col space-y-[-5px]'>
-          <Descriptions description='Product discovery and building what matters' />
-          <Descriptions description='Measuring to ensure updates are a success' />
-          <Descriptions description='And much more!' />
-        </div>
-        <Textfield
-          label='Email address'
-          placeholder='email@company.com'
-          error={error}
-          onChange={emailChangeHandler}
-          onKeyDown={handleKeyPress}
+    <div className='h-screen overflow-auto md:bg-neutralCharcoalGrey md:flex md:items-center md:justify-center'>
+      <div className='sm:hidden md:bg-neutralWhite md:w-[60vw] md:h-[60vh] md:relative md:overflow-hidden md:rounded-3xl md:flex md:flex-row  md:items-stretch md:gap-10 md:p-5'>
+        <Image
+          src={coverImageMobile}
+          alt='Cover Image Mobile'
+          priority
+          width='0'
+          height='0'
+          sizes='100vw'
+          className='block md:hidden w-full h-auto p-0 m-auto'
         />
-        <button
-          onClick={handleSubmit}
-          className='bg-neutralDarkSlateGrey p-4 h-full w-full text-neutralWhite rounded-lg font-bold spacing tracking-normal mt-6 hover:bg-gradient-to-r from-pink-600 to-primaryTomato active:bg-gradient-to-r from-pink-600 to-primaryTomato'
-        >
-          Subscribe to monthly newsletter
-        </button>
+        <div className='px-6 py-8 space-y-6 flex-1 '>
+          <h1 className='text-4xl text-neutralDarkSlateGrey font-bold'>
+            Stay updated!
+          </h1>
+          <h5 className='text-neutralDarkSlateGrey'>
+            Join 60,000+ product managers receiving monthly updates on:
+          </h5>
+          <div className='flex flex-col space-y-[-5px]'>
+            <Descriptions description='Product discovery and building what matters' />
+            <Descriptions description='Measuring to ensure updates are a success' />
+            <Descriptions description='And much more!' />
+          </div>
+          <Textfield
+            label='Email address'
+            placeholder='email@company.com'
+            error={error}
+            onChange={emailChangeHandler}
+            onKeyDown={handleKeyPress}
+          />
+          <button
+            onClick={handleSubmit}
+            className='transition ease-in-out delay-150 bg-neutralDarkSlateGrey p-4 h-full w-full md:h-fit text-neutralWhite rounded-lg font-bold spacing tracking-normal mt-6 hover:transition hover:ease-in-out hover:delay-150 hover:bg-gradient-to-r from-pink-600 to-primaryTomato active:bg-gradient-to-r from-pink-600 to-primaryTomato'
+          >
+            Subscribe to monthly newsletter
+          </button>
+        </div>
+        <Image
+          src={coverImageDesktop}
+          alt='Cover Image Desktop'
+          fill
+          className='hidden md:flex md:flex-1 md:w-full'
+        />
       </div>
     </div>
   );
